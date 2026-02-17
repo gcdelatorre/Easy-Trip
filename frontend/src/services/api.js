0aa2c447cf543ab816f5560ae725815b92721d7e
tenzies-game <giancarlo.delatorre.dev@gmail.com>
2026-02-18 00:33:07 +0800

    api notes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,6 +5,9 @@ const api = axios.create({
     withCredentials: true,
 });
 
+// this is for the frontend to attach the bearer token to the request header
+// this is only needed if domain of frontend and backend are different
+// if you have the same domain, secure cookie is enough, but we need to do this for the mobile users to work. especially for the iOS users it has a strict policy for cookies
 api.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem("token");
