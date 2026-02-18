5970fecf4afc0f215816456cc71e3237b2c8e679
tenzies-game <giancarlo.delatorre.dev@gmail.com>
2026-02-18 17:16:32 +0800

    set token in localstorage on logins. implemented hybrid auth.

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,5 +1,17 @@
 import api from "./api";
 
+// NOTES
+// we only set the token in localStorage if domain of frontend and backend are different
+// if you have the same domain, secure cookie is enough, but we need to do this for the mobile users to work. especially for the iOS users it has a strict policy for cookies
+
+
+// helper function for setting the user and token in localStorage login
+
+const setAuthData = (user, token) => {
+    localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("token", token);
+};
+
 const getCurrentUser = () => {
     const userStr = localStorage.getItem("user");
     if (userStr) return JSON.parse(userStr);
@@ -9,7 +21,7 @@ const getCurrentUser = () => {
 const loginWithGoogle = async (idToken) => {
     const response = await api.post("/auth/google", { idToken });
     if (response.data.user) {
-        localStorage.setItem("user", JSON.stringify(response.data.user));
+        setAuthData(response.data.user, response.data.token);
     }
     return response.data;
 };
@@ -17,7 +29,7 @@ const loginWithGoogle = async (idToken) => {
 const register = async (payload) => {
     const response = await api.post("/auth/register", payload);
     if (response.data.user) {
-        localStorage.setItem("user", JSON.stringify(response.data.user));
+        setAuthData(response.data.user, response.data.token);
     }
     return response.data;
 };
@@ -25,7 +37,7 @@ const register = async (payload) => {
 const loginWithEmail = async (payload) => {
     const response = await api.post("/auth/login", payload);
     if (response.data.user) {
-        localStorage.setItem("user", JSON.stringify(response.data.user));
+        setAuthData(response.data.user, response.data.token);
     }
     return response.data;
 };
@@ -33,6 +45,7 @@ const loginWithEmail = async (payload) => {
 const logout = async () => {
     await api.post("/auth/logout");
     localStorage.removeItem("user");
+    localStorage.removeItem("token");
 };
 
 const authService = {
