8c31ce9de31b674f113f6f96e48264139b1587af
tenzies-game <giancarlo.delatorre.dev@gmail.com>
2026-02-24 14:03:20 +0800

    implement lazy loading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,26 @@
 import { BrowserRouter, Routes, Route, Outlet, Navigate } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
-import LandingPage from './pages/LandingPage/LandingPage';
-import DashboardLayout from './pages/dashboard/DashboardLayout';
-import DashboardPage from './pages/dashboard/DashboardPage';
-import CreatePlanPage from './pages/dashboard/CreatePlanPage';
-import TripDetailsPage from './pages/dashboard/TripDetailsPage';
-import LoginPage from './pages/auth/LoginPage';
 import { ToastProvider } from './contexts/ToastContext';
 import { PostLoginHandler } from './components/auth/PostLoginHandler';
 import { LoadingProvider, useLoading } from './contexts/LoadingContext';
 import { FullPageSpinner } from './components/ui/customized-loading-spinner';
 import { RefreshProvider } from './contexts/RefreshContext';
 import { TravelPlanProvider } from './contexts/TravelPlanContext';
-import Explore from './pages/ExplorePage/Explore';
-import BlogDetail from './pages/ExplorePage/components/BlogDetail';
-import NotFound from './pages/NotFoundPage/NotFound';
-import AboutPage from './pages/LandingPage/AboutPage';
-import PrivacyPage from './pages/LandingPage/PrivacyPage';
-import TermsPage from './pages/LandingPage/TermsPage';
 import { TravelDetailsProvider } from './contexts/TravelDetailsContext';
+import { lazy, Suspense } from 'react';
+
+const DashboardPage = lazy(() => import('./pages/dashboard/DashboardPage'));
+const TripDetailsPage = lazy(() => import('./pages/dashboard/TripDetailsPage'));
+const CreatePlanPage = lazy(() => import('./pages/dashboard/CreatePlanPage'));
+const Explore = lazy(() => import('./pages/ExplorePage/Explore'));
+const BlogDetail = lazy(() => import('./pages/ExplorePage/components/BlogDetail'));
+const NotFound = lazy(() => import('./pages/NotFoundPage/NotFound'));
+const AboutPage = lazy(() => import('./pages/LandingPage/AboutPage'));
+const PrivacyPage = lazy(() => import('./pages/LandingPage/PrivacyPage'));
+const TermsPage = lazy(() => import('./pages/LandingPage/TermsPage'));
+const LandingPage = lazy(() => import('./pages/LandingPage/LandingPage'));
+const LoginPage = lazy(() => import('./pages/auth/LoginPage'));
+const DashboardLayout = lazy(() => import('./pages/dashboard/DashboardLayout'));
 
 export default function App() {
   return (
@@ -45,29 +47,30 @@ function AppContent() {
 
   return (
     <>
-      <FullPageSpinner isOpen={loading} message={message} />
-      <PostLoginHandler />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<LoginPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/privacy" element={<PrivacyPage />} />
-        <Route path="/terms" element={<TermsPage />} />
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/explore/:slug" element={<BlogDetail />} />
-        <Route path="create" element={<CreatePlanPage />} />
-        <Route path='/not-found' element={<NotFound />} />
+      <Suspense fallback={<FullPageSpinner isOpen={true} message="Loading Page..." />}>
+        <FullPageSpinner isOpen={loading} message={message} />
+        <PostLoginHandler />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/privacy" element={<PrivacyPage />} />
+          <Route path="/terms" element={<TermsPage />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/explore/:slug" element={<BlogDetail />} />
+          <Route path="create" element={<CreatePlanPage />} />
+          <Route path='/not-found' element={<NotFound />} />
 
-        {/* Dashboard Routes with Layout */}
-        <Route path="/dashboard" element={<DashboardLayout children={<Outlet />} />}>
-          <Route index element={<DashboardPage />} />
-          <Route path="trip/:tripId" element={<TripDetailsPage />} />
-        </Route>
+          {/* Dashboard Routes with Layout */}
+          <Route path="/dashboard" element={<DashboardLayout children={<Outlet />} />}>
+            <Route index element={<DashboardPage />} />
+            <Route path="trip/:tripId" element={<TripDetailsPage />} />
+          </Route>
 
-        {/* Catch-all 404 Route */}
-        <Route path="*" element={<Navigate to="/not-found" replace />} />
-      </Routes>
+          {/* Catch-all 404 Route */}
+          <Route path="*" element={<Navigate to="/not-found" replace />} />
+        </Routes>
+      </Suspense>
     </>
   )
 }
